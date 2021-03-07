8d75da68ad0554fbf21d8507625844ba5805af9d
Pedro Luz <pedromsluz@gmail.com>
2021-03-07 15:10:33 +0000

    generating folders ok

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Narven/blueprint
 go 1.15
 
 require (
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,9 +2,10 @@ package app
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"os/exec"
 
+	"github.com/fatih/color"
 	"gopkg.in/yaml.v2"
 )
 
@@ -17,38 +18,68 @@ type T struct {
 }
 
 func Parse(data []byte) {
-	t := &T{}
+	bad := color.New(color.FgRed).PrintlnFunc()
+	info := color.New(color.FgCyan).PrintlnFunc()
+	log := color.New(color.FgHiCyan).PrintlnFunc()
+	ok := color.New(color.FgHiGreen).PrintlnFunc()
+
+	t := T{}
 
 	err := yaml.Unmarshal(data, &t)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		bad("error: %v", err)
+		return
 	}
 	//fmt.Printf("--- t:\n%v\n\n", t)
 
 	d, err := yaml.Marshal(&t)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		bad(err.Error())
+		log("error: %v", err)
+		return
 	}
-	fmt.Printf("--- t dump:\n%s\n\n", string(d))
 
-	fmt.Println(t.Path)
+	info(string(d))
 
-	if t.Path != nil {
+	if t.Path != nil { // Path is defined
+		// let check if the path exists
+		if _, err := os.Stat(*t.Path); os.IsNotExist(err) {
+			info(fmt.Printf("Path does not exists: %s", *t.Path))
+			if err := os.MkdirAll(*t.Path, 0777); err != nil {
+				bad(fmt.Printf("\tError creating path: %s", *t.Path))
+				bad(err.Error())
+				os.Exit(1)
+			}
+			info(fmt.Printf("Path created: %s", *t.Path))
+		}
+
 		if err := os.Chdir(*t.Path); err != nil {
-			log.Fatal(err)
+			bad(fmt.Printf("Cannot change to path: %s", *t.Path))
+			bad(err.Error())
+			os.Exit(1)
 		}
+	} else {
+		// A path is not defined, assuming current directory
+		// we can remove this else later on...
+		current, _ := os.Getwd()
+		t.Path = &current
+		info(fmt.Printf("Path: %s", current))
 	}
 
-	for _, f := range t.Structure {
-		path := fmt.Sprintf("%s/%s", t.Output, f)
-		if err := os.MkdirAll(path, os.ModePerm); err != nil {
-			log.Fatal(err)
+	for _, path := range t.Structure {
+		log("Trying path:", path)
+		if err := os.MkdirAll(path, 0777); err != nil {
+			bad(err.Error())
 		}
+		ok("\tCreated:", path)
+	}
 
-		if err := os.Mkdir(path, os.ModeDir); err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println("> Path:", path)
+	c, _ := os.Getwd()
+	log("Final structure", c)
+	cmd := exec.Command("tree")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		bad(err.Error())
 	}
 }
